99752db78553ae5704ab6140cd1a92d2cdd4cd44
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-03-24 21:20:17 +0100

    Add Market and MarbleColour implementation.

--- src/main/java/it/polimi/ingsw/server/model/gamecontext/market/Market.java
+++ src/main/java/it/polimi/ingsw/server/model/gamecontext/market/Market.java
@@ -2,30 +2,51 @@ package it.polimi.ingsw.server.model.gamecontext.market;
 
 import it.polimi.ingsw.server.model.gameitems.MarbleColour;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class Market {
 
-	Random randGenerator = new Random();
+	Random randGenerator;
 
 	MarbleColour[][] matrix;
 	MarbleColour outMarble;
+	private int nRows;
+	private int nColumns;
 
 	public Market(int nRows, int nColumns, Map<MarbleColour,Integer> marbles) throws WrongNumberOfMarblesException {
+		randGenerator = new Random();
+		initializeMatrixAndOutMarble(nRows, nColumns, marbles);
+		}
+
+	public Market(Random randGenerator, int nRows, int nColumns, Map<MarbleColour,Integer> marbles) throws WrongNumberOfMarblesException {
+		this.randGenerator = randGenerator;
+		initializeMatrixAndOutMarble(nRows, nColumns, marbles);
+	}
+
+	private void initializeMatrixAndOutMarble(int nRows, int nColumns, Map<MarbleColour,Integer> marbles) throws WrongNumberOfMarblesException{
+		this.nRows = nRows;
+		this. nColumns = nColumns;
+
 		matrix = new MarbleColour[nRows][nColumns];
 		if (marbles.values().stream().mapToInt(i -> i).sum() != nRows*nColumns + 1) //marbles in matrix + 1 marble out
 			throw new WrongNumberOfMarblesException();
-		int i=0;
-		//TODO
-        // To generate a random number from [0, N) use randGenerator.nextInt(N)
-	}
 
-	public Market(Random randGenerator, int nRows, int nColumns, Map<MarbleColour,Integer> marbles) throws WrongNumberOfMarblesException {
-		this(nRows, nColumns, marbles);
-		this.randGenerator = randGenerator;
-	}
+		List<MarbleColour> marblesList = new ArrayList<>();
+		for(MarbleColour m : marbles.keySet() )
+			for (int v = 0; v < marbles.get(m); v++)
+				marblesList.add(m);
 
+		for(int r=0; r<nRows; r++)
+			for(int c=0; c<nColumns; c++) {
+				int randNum = randGenerator.nextInt(marblesList.size());
+				matrix[r][c] = marblesList.remove(randNum);
+			}
+
+		outMarble = marblesList.get(0);
+	}
 
 	public MarbleColour[ ][ ] getMarbleMatrix() {
 		return matrix;
@@ -36,11 +57,33 @@ public class Market {
 	}
 
 	public MarbleColour[ ] fetchMarbleRow(int row) {
-		return null;
+		MarbleColour[] marbleRow = new MarbleColour[nColumns];
+
+		for(int c=0; c<nColumns; c++)
+			marbleRow[c] = matrix[row][c];
+
+		for(int c=0; c<nColumns-1; c++)
+			matrix[row][c] = matrix[row][c+1];
+
+		matrix[row][nColumns-1] = outMarble;
+
+		outMarble = marbleRow[0];
+
+		return marbleRow;
 	}
 
 	public MarbleColour[ ] fetchMarbleColumn(int column) {
-		return null;
-	}
+		MarbleColour[] marbleColumn = new MarbleColour[nRows];
 
+		for(int r=0; r<nRows; r++)
+			marbleColumn[r] = matrix[r][column];
+
+		for(int r=0; r<nRows-1; r++)
+			matrix[r][column] = matrix[r+1][column];
+		matrix[nRows-1][column] = outMarble;
+
+		outMarble = marbleColumn[0];
+
+		return marbleColumn;
+	}
 }
--- src/main/java/it/polimi/ingsw/server/model/gameitems/MarbleColour.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/MarbleColour.java
@@ -3,25 +3,44 @@ package it.polimi.ingsw.server.model.gameitems;
 import java.util.Optional;
 
 public class MarbleColour {
+	private Optional<ResourceType> resourceType;
+	private int faithPoints;
+	private boolean isSpecialMarble;
+	private String marbleID;
 
 	public MarbleColour(Optional<ResourceType> resourceType, int faithPoints, boolean isSpecialMarble, String marbleID) {
-
+		this.resourceType = resourceType;
+		this.faithPoints = faithPoints;
+		this.isSpecialMarble = isSpecialMarble;
+		this.marbleID = marbleID;
 	}
 
 	public Optional<ResourceType> getResourceType() {
-		return null;
+		return resourceType;
 	}
 
 	public int getFaithPoints() {
-		return 0;
+		return faithPoints;
 	}
 
 	public boolean isSpecialMarble() {
-		return false;
+		return isSpecialMarble;
 	}
 
 	public String getMarbleID() {
-		return null;
+		return marbleID;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof MarbleColour)) return false;
+		MarbleColour m = (MarbleColour) o;
+		return (marbleID.equals(m.marbleID));
+	}
+
+	@Override
+	public int hashCode() {
+		return marbleID.hashCode();
 	}
 
 }
