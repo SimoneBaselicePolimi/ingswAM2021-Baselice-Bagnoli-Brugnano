3e282c6f218f09ffb32e559b09289a68a2840a8d
NataliaBagnoli <natalia.bagnoli@mail.polimi.it>
2021-03-25 16:12:32 +0100

    Add ResourceStorage and ResourceStorageBuilder JavaDoc

--- src/main/java/it/polimi/ingsw/server/model/storage/ResourceStorage.java
+++ src/main/java/it/polimi/ingsw/server/model/storage/ResourceStorage.java
@@ -8,13 +8,28 @@ import java.util.List;
 import java.util.Map;
 
 public class ResourceStorage {
+	/**
+	 * List of rules that the storage implements
+	 */
 	private List<ResourceStorageRule> rules;
+	/**
+	 * Type and number of resources that are present in the storage
+	 */
 	private Map<ResourceType,Integer> resources = new HashMap<>();;
 
+	/**
+	 * ResourceStorage Constructor
+	 * @param rules
+	 */
 	protected ResourceStorage(List<ResourceStorageRule> rules){
 		this.rules = rules;
 	}
 
+	/**
+	 * Add new resources (type and number) in resources that contains all the resources of the storage
+	 * @param newResources Map of resources that the method adds to the storage
+	 * @throws ResourceStorageRuleViolationException if new resources don't respect rules of the storage
+	 */
 	public void addResources(Map<ResourceType,Integer> newResources) throws ResourceStorageRuleViolationException {
 		checkRules(newResources);
 		for (ResourceType resource : newResources.keySet()) {
@@ -25,6 +40,12 @@ public class ResourceStorage {
 		}
 	}
 
+	/**
+	 * Remove some resources (type and number) from the storage
+	 * @param newResources Map of resources that the method removes from the storage
+	 * @return newResources Map of resources that the method removes from the storage
+	 * @throws NotEnoughResourcesException if there aren't enough resources to remove
+	 */
 	public Map<ResourceType, Integer> removeResources(Map<ResourceType, Integer> newResources)
 			throws NotEnoughResourcesException {
 		for (ResourceType resource : newResources.keySet()) {
@@ -40,6 +61,11 @@ public class ResourceStorage {
 		return newResources;
 	}
 
+	/**
+	 * check if new resources respect rules of the storage
+	 * @param resources
+	 * @throws ResourceStorageRuleViolationException if new resources don't respect rules of the storage
+	 */
 	private void checkRules(Map<ResourceType, Integer> resources) throws ResourceStorageRuleViolationException{
 		for(ResourceStorageRule rule : rules){
 			if (!rule.checkRule(this, resources))
@@ -47,6 +73,9 @@ public class ResourceStorage {
 		}
 	}
 
+	/**
+	 * @return Type and number of resources in the storage
+	 */
 	public Map<ResourceType, Integer> peekResources() {
 		return this.resources;
 	}
--- src/main/java/it/polimi/ingsw/server/model/storage/ResourceStorageBuilder.java
+++ src/main/java/it/polimi/ingsw/server/model/storage/ResourceStorageBuilder.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResourceStorageBuilder {
+	/**
+	 * List of rules that the storage implements when the storage is created
+	 */
 	private List<ResourceStorageRule> rules = new ArrayList<>();;
 
+	/**
+	 * Create new ResourceStorageBuilder
+	 * @return ResourceStorageBuilder
+	 */
 	public static ResourceStorageBuilder initResourceStorageBuilder() {
 		return new ResourceStorageBuilder();
 	}
 
+	/**
+	 * Create new ResourceStorage with its respective rules
+	 * @return ResourceStorage
+	 */
 	public ResourceStorage createResourceStorage() {
 		return new ResourceStorage(rules);
 	}
 
+	/**
+	 * Add one new rule to the list that contains all the rules of the storage
+	 * @param rule
+	 * @return ResourceStorageBuilder
+	 */
 	public ResourceStorageBuilder addRule(ResourceStorageRule rule) {
 		rules.add(rule);
 		return this;
