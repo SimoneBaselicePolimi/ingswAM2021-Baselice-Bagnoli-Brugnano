264b5775cd309a62a9878215381e7b1b7ab5deed
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-03-28 20:39:23 +0200

    Add Production, ProductionDiscount, WhiteMarbleSubstitution, ResourceType implementation and JavaDoc.

--- src/main/java/it/polimi/ingsw/server/model/gameitems/ProductionDiscount.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/ProductionDiscount.java
@@ -1,15 +1,45 @@
 package it.polimi.ingsw.server.model.gameitems;
 
-import java.util.Map;
-
+/**
+ * This class represents the Production Discount power held by some Game Items (Leader Cards).
+ * A Player who owns this Production Discount Item, when buying a Development Card, can pay its cost
+ * with a discount of the indicated Resource (if the card he's buying has that Resource type as a cost).
+*/
 public class ProductionDiscount {
+	/**
+	 * Resource type the Player can discount from the cost of a Development Card
+	 */
+	private ResourceType resourceType;
 
-	public ResourceType getResourceTypeToDiscount() {
-		return null;
+	/**
+	 * Number of resources the Player can discount from the cost of a Development Card
+	 */
+	private int getDiscount;
+
+	/**
+	 * Class constructor.
+	 * @param resourceType resource the Player can discount from the cost
+	 * @param getDiscount number of resources the Player can discount from the cost
+	 */
+	public ProductionDiscount(ResourceType resourceType, int getDiscount) {
+		this.resourceType = resourceType;
+		this.getDiscount = getDiscount;
 	}
 
+	/**
+	 * Method to get the type of resources the Player can discount from the cost of a Development Card.
+	 * @return type of resources discounted
+	 */
+	public ResourceType getResourceTypeToDiscount() {
+		return resourceType;
+	}
+
+	/**
+	 * Method to get the number of resources the Player can discount from the cost of a Development Card.
+	 * @return number of resources discounted
+	 */
 	public int getDiscount() {
-		return 0;
+		return getDiscount;
 	}
 
 }
--- src/main/java/it/polimi/ingsw/server/model/gameitems/WhiteMarbleSubstitution.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/WhiteMarbleSubstitution.java
@@ -1,11 +1,30 @@
 package it.polimi.ingsw.server.model.gameitems;
 
-import java.util.List;
-
+/**
+ * This class represents the White Marble Substitution power held by some Game Items (Leader Cards).
+ * A Player who owns this Substitution Item, when taking Resources from the Market, can choose which Resource to take
+ * (from those given by this Substitution Item) for each of the white Marbles.
+ */
 public class WhiteMarbleSubstitution {
+	/**
+	 * Resource type the Player can substitute to a White Marble taken from the Market
+	 */
+	private ResourceType resourceTypeToSubstitute;
 
-	MarbleColour getResourceTypeToSubstitute() {
-		return null;
+	/**
+	 * Class constructor.
+	 * @param resourceTypeToSubstitute resource type to substitute to a White Marble
+	 */
+	public WhiteMarbleSubstitution(ResourceType resourceTypeToSubstitute) {
+		this.resourceTypeToSubstitute = resourceTypeToSubstitute;
+	}
+
+	/**
+	 * Method to get the specific Resource type which can be substituted to a White Marble.
+	 * @return resource type to substitute to a White Marble
+	 */
+	ResourceType getResourceTypeToSubstitute() {
+		return resourceTypeToSubstitute;
 	}
 
 }
