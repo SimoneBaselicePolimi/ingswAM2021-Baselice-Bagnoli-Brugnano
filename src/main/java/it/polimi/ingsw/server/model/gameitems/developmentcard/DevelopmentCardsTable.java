d95379b475bdf3dc13c7f44b64242951cfc5abe6
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-01 00:54:33 +0200

    Implement DevelopmentCardsTable constructor and other methods

--- src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardsTable.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardsTable.java
@@ -2,24 +2,54 @@ package it.polimi.ingsw.server.model.gameitems.developmentcard;
 
 import it.polimi.ingsw.server.model.gameitems.cardstack.ShuffledCardDeck;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DevelopmentCardsTable {
 
-	private Map<DevelopmentCardLevel, Map<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>>> cards;
+	private Map<DevelopmentCardLevel, Map<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>>> cards = new HashMap<DevelopmentCardLevel, Map<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>>>();
 
-	public DevelopmentCardsTable(List<DevelopmentCard> cards) {
-
+	public DevelopmentCardsTable(List<DevelopmentCard> developmentCards) {
+		Map <DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>> mapColors = new HashMap<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>>();
+		for (DevelopmentCard card : developmentCards){
+			for (DevelopmentCardLevel level : card.getLevel().values()){
+				for (DevelopmentCardColour colour : card.getColour().values()){
+					List <DevelopmentCard> deck = new ArrayList<DevelopmentCard>();
+					deck.add(card);
+					ShuffledCardDeck<DevelopmentCard> shuffledCardDeck = new ShuffledCardDeck(deck);
+					mapColors.put(colour,shuffledCardDeck);
+					}
+				cards.put(level, mapColors);
+				}
+			}
 	}
 
 	public List<DevelopmentCard> getAvailableCards() {
-		return null;
+	List<DevelopmentCard> availableCards = new ArrayList<DevelopmentCard>();
+		for (DevelopmentCardLevel level : cards.keySet()){
+			for (Map<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>> value : cards.values()) {
+				for (DevelopmentCardColour colour : value.keySet()) {
+					for (ShuffledCardDeck<DevelopmentCard> deck : value.values())
+						availableCards.add(deck.peek());
+				}
+			}
+		}
+		return availableCards;
 	}
 
-//toglie primo elemento e lo restituisce
 	public DevelopmentCard popCard(DevelopmentCardLevel level, DevelopmentCardColour colour) {
-		return null;
+		for (DevelopmentCardLevel cardlevel : cards.keySet()) {
+			for (Map<DevelopmentCardColour, ShuffledCardDeck<DevelopmentCard>> value : cards.values()) {
+				for (DevelopmentCardColour cardColour : value.keySet()) {
+					for (ShuffledCardDeck <DevelopmentCard> deck : value.values()) {
+						if (cardlevel == level && cardColour == colour)
+							return(deck.pop());
+					}
+				}
+			}
+		}
 	}
 
 	public Map<DevelopmentCardLevel,Map<DevelopmentCardColour,DevelopmentCard>> getAvailableCardsAsMap() {
---
+++ src/test/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardsTableTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.server.model.gameitems.developmentcard;
+
+import it.polimi.ingsw.server.model.gameitems.ResourceType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.verify;
+public class DevelopmentCardsTableTest {
+    DevelopmentCard developmentCard = new DevelopmentCard()
+}
