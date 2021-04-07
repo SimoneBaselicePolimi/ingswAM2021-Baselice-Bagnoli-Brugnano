4853d06a02097497e2e33c20562de2fc0526b8ca
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-04-07 17:10:36 +0200

    Add configuration files: faith-path, leader-cards, marbles, market. Add serialization and deserialization methods in DevelopmentCardColour and ResourceType enums.

--- src/main/java/it/polimi/ingsw/server/model/gameitems/ResourceType.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/ResourceType.java
@@ -4,8 +4,26 @@ package it.polimi.ingsw.server.model.gameitems;
  * This enumeration lists all the Resource types used in the Game.
  */
 public enum ResourceType {
-    COINS,
-    STONES,
-    SERVANTS,
-    SHIELDS
+    COINS("COINS"),
+    STONES("STONES"),
+    SERVANTS("SERVANTS"),
+    SHIELDS("SHIELDS");
+
+    String serializationName;
+
+    ResourceType(String serializationName) {
+        this.serializationName = serializationName;
+    }
+
+    public String serialize() {
+        return this.serializationName;
+    }
+
+    public static ResourceType deserialize(String serializationName) {
+        for(ResourceType resourceType : ResourceType.values())
+            if(resourceType.serializationName.equals(serializationName))
+                return resourceType;
+        throw new IllegalArgumentException("Deserialization failed: invalid serialization name");
+    }
+
 }
--- src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardColour.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardColour.java
@@ -1,10 +1,29 @@
 package it.polimi.ingsw.server.model.gameitems.developmentcard;
+
 /**
- * This class represents the colour of a development card (GREEN, BLUE, YELLOW, PURPLE)
+ * This enumeration lists all the Development Card Colours present in the Game.
  */
 public enum DevelopmentCardColour {
-	GREEN,
-	BLUE,
-	YELLOW,
-	PURPLE
-}
+	GREEN("GREEN"),
+	BLUE("BLUE"),
+	YELLOW("YELLOW"),
+	PURPLE("PURPLE");
+
+	String serializationName;
+
+	DevelopmentCardColour(String serializationName) {
+		this.serializationName = serializationName;
+	}
+
+	public String serialize() {
+		return this.serializationName;
+	}
+
+	public static DevelopmentCardColour deserialize(String serializationName) {
+		for(DevelopmentCardColour cardColour : DevelopmentCardColour.values())
+			if(cardColour.serializationName.equals(serializationName))
+				return cardColour;
+		throw new IllegalArgumentException("Deserialization failed: invalid serialization name");
+	}
+
+	}
