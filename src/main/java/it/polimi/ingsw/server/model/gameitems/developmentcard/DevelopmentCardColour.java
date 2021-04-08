47356222969c884de330f6eac0c9a60e42435165
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-08 09:12:04 +0200

    Change indentation

--- src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardColour.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardColour.java
@@ -3,5 +3,8 @@ package it.polimi.ingsw.server.model.gameitems.developmentcard;
  * This class represents the colour of a development card (GREEN, BLUE, YELLOW, PURPLE)
  */
 public enum DevelopmentCardColour {
-	GREEN, BLUE, YELLOW, PURPLE
+	GREEN,
+	BLUE,
+	YELLOW,
+	PURPLE
 }
--- src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardLevel.java
+++ src/main/java/it/polimi/ingsw/server/model/gameitems/developmentcard/DevelopmentCardLevel.java
@@ -4,5 +4,7 @@ package it.polimi.ingsw.server.model.gameitems.developmentcard;
  * This class represents the level of a development card (FIRST_LEVEL, SECOND_LEVEL, THIRD_LEVEL)
  */
 public enum DevelopmentCardLevel {
-	FIRST_LEVEL, SECOND_LEVEL, THIRD_LEVEL
+	FIRST_LEVEL,
+	SECOND_LEVEL,
+	THIRD_LEVEL
 }
