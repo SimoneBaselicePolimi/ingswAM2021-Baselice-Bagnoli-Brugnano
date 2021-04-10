80dda151117db6c1c297d2fee2193d9b8ca43ed7
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-10 23:29:15 +0200

    INGSW-17 start to implement testRandomShuffle in GameSetupStateTest

--- src/test/java/it/polimi/ingsw/server/model/gamemanager/gamestate/GameSetupStateTest.java
+++ src/test/java/it/polimi/ingsw/server/model/gamemanager/gamestate/GameSetupStateTest.java
@@ -1,32 +1,50 @@
 package it.polimi.ingsw.server.model.gamemanager.gamestate;
 
+import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.server.model.gamecontext.GameContext;
 import it.polimi.ingsw.server.model.gameitems.leadercard.LeaderCard;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import java.util.HashSet;
-import java.util.Set;
+import static org.mockito.Mockito.lenient;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
 
 public class GameSetupStateTest {
+
+    Player player1 = new Player("first");
+    Player player2 = new Player("second");
+    Player player3 = new Player("third");
+    Player player4 = new Player("fourth");
+
+    List<Player> players= List.of(player1, player2, player3, player4);
 
     @Mock
     GameContext gameContext;
 
-    @Mock
-    LeaderCard card1;
-    LeaderCard card2;
-    LeaderCard card3;
-    LeaderCard card4;
-    LeaderCard card5;
-    LeaderCard card6;
-    LeaderCard card7;
-    LeaderCard card8;
-    LeaderCard card9;
-    LeaderCard card10;
-    LeaderCard card11;
-    LeaderCard card12;
+    @BeforeEach
+    void setUp() {
+        lenient().when(gameContext.getPlayersTurnOrder()).thenReturn(players);
+    }
 
+    LeaderCard card1 = new LeaderCard( null, null, null, null, null, 1);
+    LeaderCard card2 = new LeaderCard( null, null, null, null, null, 2);
+    LeaderCard card3 = new LeaderCard( null, null, null, null, null, 3);
+    LeaderCard card4 = new LeaderCard( null, null, null, null, null, 4);
+    LeaderCard card5 = new LeaderCard( null, null, null, null, null, 5);
+    LeaderCard card6 = new LeaderCard( null, null, null, null, null, 6);
+    LeaderCard card7 = new LeaderCard( null, null, null, null, null, 7);
+    LeaderCard card8 = new LeaderCard( null, null, null, null, null, 8);
+    LeaderCard card9 = new LeaderCard( null, null, null, null, null, 9);
+    LeaderCard card10 = new LeaderCard( null, null, null, null, null, 1);
+    LeaderCard card11 = new LeaderCard( null, null, null, null, null, 2);
+    LeaderCard card12 = new LeaderCard( null, null, null, null, null, 3);
 
     Set <LeaderCard> leaderCards = Set.of(
                 card1,
@@ -45,7 +63,12 @@ public class GameSetupStateTest {
 
     @Test
     void testRandomShuffle(){
-        GameSetupState state = new GameSetupState(gameContext, leaderCards, 3);
+        GameSetupState state1 = new GameSetupState(new Random(1),gameContext, leaderCards, 3);
+        GameSetupState state1Copy = new GameSetupState(new Random(1),gameContext, leaderCards, 3);
+        GameSetupState state2 = new GameSetupState(new Random(2),gameContext, leaderCards, 2);
+        GameSetupState state3 = new GameSetupState(new Random(2),gameContext, leaderCards, 4);
+
+        //assertEquals(state1.getLeaderCardsGivenToThePlayers(), state1Copy.getLeaderCardsGivenToThePlayers());
     }
 }
 
