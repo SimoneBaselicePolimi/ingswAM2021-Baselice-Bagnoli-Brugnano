2216fdc6ac3900ee7b21bdedae719497bcfef080
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-17 14:00:41 +0200

    INGSW-17 add GameHistory implementation

--- src/main/java/it/polimi/ingsw/server/model/gamehistory/GameHistory.java
+++ src/main/java/it/polimi/ingsw/server/model/gamehistory/GameHistory.java
@@ -1,9 +1,22 @@
 package it.polimi.ingsw.server.model.gamehistory;
 
+import it.polimi.ingsw.logger.LogLevel;
+import it.polimi.ingsw.logger.ProjectLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
 public class GameHistory {
 
-	public GameAction getGameHistory() {
-		return null;
+	private List<GameAction> gameActions = new ArrayList<>();
+
+	public List<GameAction> getGameHistory() {
+		return new ArrayList<>(gameActions);
+	}
+
+	public void addAction(GameAction gameAction){
+		gameActions.add(gameAction);
 	}
 
 }
---
+++ src/test/java/it/polimi/ingsw/server/model/gamehistory/GameHistoryTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.server.model.gamehistory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameHistoryTest {
+
+    @Test
+    void getGameHistory() {
+    }
+}
