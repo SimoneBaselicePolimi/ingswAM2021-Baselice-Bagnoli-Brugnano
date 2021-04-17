af0b8f4d0cbaded91d0b3fc0229ab37563006e17
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-17 14:06:57 +0200

    INGSW-17 add GameAction

--- src/main/java/it/polimi/ingsw/server/model/gamehistory/GameAction.java
+++ src/main/java/it/polimi/ingsw/server/model/gamehistory/GameAction.java
@@ -1,5 +1,11 @@
 package it.polimi.ingsw.server.model.gamehistory;
 
-public class GameAction {
+import it.polimi.ingsw.localization.Localization;
+import it.polimi.ingsw.server.model.gameitems.ResourceType;
+import it.polimi.ingsw.server.model.storage.ResourceStorage;
 
+import java.util.Map;
+
+public abstract class GameAction {
+    public abstract String getActionMessage();
 }
