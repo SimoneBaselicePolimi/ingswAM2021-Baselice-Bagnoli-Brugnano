48775c1aca2aaf9423cf3f44eefb98c958c96951
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-20 10:18:53 +0200

    INGSW-17 add leaderCardsThePlayerWantsToDiscard and leaderCardsThePlayerWantsToActivate to LeaderActionClientRequest class

--- src/main/java/it/polimi/ingsw/network/clientrequest/LeaderActionClientRequest.java
+++ src/main/java/it/polimi/ingsw/network/clientrequest/LeaderActionClientRequest.java
@@ -1,18 +1,34 @@
 package it.polimi.ingsw.network.clientrequest;
 
 import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.gameitems.leadercard.LeaderCard;
+import it.polimi.ingsw.server.model.gameitems.leadercard.LeaderCardRequirementsNotSatisfiedException;
 import it.polimi.ingsw.server.model.gamemanager.gamestate.GameState;
 import it.polimi.ingsw.network.servermessage.ServerMessage;
+import it.polimi.ingsw.utils.serialization.SerializeAsSetOfIds;
 
 import java.util.Map;
+import java.util.Set;
 
 public class LeaderActionClientRequest extends ClientRequest {
+    @SerializeAsSetOfIds
+    public final Set<LeaderCard> leaderCardsThePlayerWantsToDiscard;
 
-    public LeaderActionClientRequest(Player player) {
+    @SerializeAsSetOfIds
+    public final Set<LeaderCard> leaderCardsThePlayerWantsToActivate;
+
+
+    public LeaderActionClientRequest(
+        Player player,
+        Set<LeaderCard> leaderCardsThePlayerWantsToDiscard,
+        Set<LeaderCard> leaderCardsThePlayerWantsToActivate
+    ) {
         super(player);
+        this.leaderCardsThePlayerWantsToDiscard = leaderCardsThePlayerWantsToDiscard;
+        this.leaderCardsThePlayerWantsToActivate = leaderCardsThePlayerWantsToActivate;
     }
 
-    public Map<Player, ServerMessage> callHandler(GameState state) {
+    public Map<Player, ServerMessage> callHandler(GameState state) throws LeaderCardRequirementsNotSatisfiedException {
 		return(state.handleRequestLeaderAction(this));
 	}
 
