c48bf5cc1b1ccf86eb542b65477c96c53563a457
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-04-25 18:17:07 +0200

    INGSW-17 add PostTurnFinalAction and PostTurnInitialAction classes

---
+++ src/main/java/it/polimi/ingsw/server/model/gamehistory/PostTurnFinalAction.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.server.model.gamehistory;
+
+import it.polimi.ingsw.localization.Localization;
+import it.polimi.ingsw.server.model.Player;
+
+public class PostTurnFinalAction extends GameAction{
+    private final Player player;
+
+    public PostTurnFinalAction(Player player) {
+        this.player = player;
+    }
+
+    @Override
+    public String getActionMessage() {
+        return Localization.getLocalization().getString(
+            "postTurn.finalMessage",
+            player.getName()
+        );
+    }
+}
