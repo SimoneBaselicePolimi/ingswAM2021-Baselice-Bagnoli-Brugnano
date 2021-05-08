256c13902d271929da7cfed1c13f4ffd457fcbc2
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-05-08 12:54:22 +0200

    INGSW-17 start to implement InitialChoicesClientRequestValidatorTest

--- src/test/java/it/polimi/ingsw/network/clientrequest/validator/InitialChoicesClientRequestValidatorTest.java
+++ src/test/java/it/polimi/ingsw/network/clientrequest/validator/InitialChoicesClientRequestValidatorTest.java
@@ -1,31 +1,201 @@
 package it.polimi.ingsw.network.clientrequest.validator;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import it.polimi.ingsw.network.clientrequest.ManageResourcesFromMarketClientRequest;
+import it.polimi.ingsw.configfile.GameInfoConfig;
+import it.polimi.ingsw.configfile.GameRules;
+import it.polimi.ingsw.network.clientrequest.InitialChoicesClientRequest;
 import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.server.model.gamecontext.GameContext;
 import it.polimi.ingsw.server.model.gamecontext.playercontext.PlayerContext;
-import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
 import it.polimi.ingsw.server.model.gameitems.ResourceType;
+import it.polimi.ingsw.server.model.gameitems.leadercard.LeaderCard;
 import it.polimi.ingsw.server.model.gamemanager.GameManager;
 import it.polimi.ingsw.server.model.storage.ResourceStorage;
+import it.polimi.ingsw.testutils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 
 class InitialChoicesClientRequestValidatorTest {
 
+    @Mock
+    ResourceStorage storage1;
+
+    @Mock
+    ResourceStorage storage2;
+
+    @Mock
+    PlayerContext playerContext;
+
+    Map<ResourceStorage, Map<ResourceType, Integer>> twoResourcesChosen = new HashMap<>();
+    Map<ResourceStorage, Map<ResourceType, Integer>> fiveResourcesChosen = new HashMap<>();
+    Map<ResourceStorage, Map<ResourceType, Integer>> invalidStorageWithOneResourceChosen = new HashMap<>();
+
+    Player player1 = new Player("first");
+    Player player2 = new Player("second");
+
+    List<Player> playersInOrder = List.of(player1, player2);
+
+    @Mock
+    GameManager gameManager;
+
+    @Mock
+    GameContext gameContext;
+
+    Set<LeaderCard> leaderCardsGivenToThePlayer;
+
+    Iterator<LeaderCard> iter;
+    LeaderCard rightCard1;
+    LeaderCard rightCard2;
+
+    @Mock
+    LeaderCard differentCard;
+
+    int numberOfLeadersCardsGivenToThePlayer = 4;
+    int numberOfLeadersCardsThePlayerKeeps = 2;
+
+    InitialChoicesClientRequestValidator validator = new InitialChoicesClientRequestValidator();
+
+    GameRules gameRules;
+
+    Map<Player, Integer> playerInitialFaithPoints = Map.of(
+        player1, 0,
+        player2, 1
+    );
+
+    Map<Player, Integer> playerInitialStarResources = Map.of(
+        player1, 1,
+        player2, 5
+    );
+
+    int maxNumberOfPlayers = 3;
+
+    @BeforeEach
+    void setUp() {
+
+        leaderCardsGivenToThePlayer = TestUtils.generateSetOfMockWithID(LeaderCard.class, 4);
+        iter = leaderCardsGivenToThePlayer.iterator();
+        rightCard1 = iter.next();
+        rightCard2 = iter.next();
+
+        Map<Integer, GameInfoConfig.GameSetup.InitialPlayerResourcesAndFaithPoints> initialResources = new HashMap<>();
+        for (int i = 0; i < playersInOrder.size(); i++){
+            Player player = playersInOrder.get(i);
+            initialResources.put(
+                i + 1,
+                new GameInfoConfig.GameSetup.InitialPlayerResourcesAndFaithPoints(
+                    playerInitialStarResources.get(player),
+                    playerInitialFaithPoints.get(player))
+            );
+        }
+
+        gameRules = new GameRules(
+            new GameInfoConfig(
+                maxNumberOfPlayers,
+                false,
+                new GameInfoConfig.GameSetup(
+                    numberOfLeadersCardsGivenToThePlayer,
+                    numberOfLeadersCardsThePlayerKeeps,
+                    initialResources
+                ),
+                0,
+                null,
+                null,
+                false,
+                false
+            ),
+            null,
+            null,
+            null,
+            null
+        );
+
+        lenient().when(gameManager.getGameContext()).thenReturn(gameContext);
+        lenient().when(gameManager.getGameRules()).thenReturn(gameRules);
+        lenient().when(gameContext.getPlayerContext(player1)).thenReturn(playerContext);
+        lenient().when(gameContext.getPlayerContext(player2)).thenReturn(playerContext);
+        lenient().when(gameContext.getPlayersTurnOrder()).thenReturn(playersInOrder);
+        lenient().when(gameManager.getPlayers()).thenReturn(new HashSet<>(playersInOrder));
+        lenient().when(storage1.canAddResources(any())).thenReturn(false);
+        lenient().when(storage2.canAddResources(any())).thenReturn(true);
+        lenient().when(leaderCardsGivenToThePlayer.contains(differentCard)).thenReturn(false);
+
+        twoResourcesChosen.put(storage2, Map.of(ResourceType.STONES, 1, ResourceType.SERVANTS, 1));
+        fiveResourcesChosen.put(storage2, Map.of(ResourceType.SHIELDS, 3, ResourceType.COINS, 2));
+        invalidStorageWithOneResourceChosen.put(storage1, Map.of(ResourceType.COINS, 1));
+    }
+
+    @Test
+
+    void getValidatorFromClientRequest() {
+        InitialChoicesClientRequest request = new InitialChoicesClientRequest(
+            player1,
+            Set.of(rightCard1, rightCard2),
+            twoResourcesChosen
+        );
+
+        assertTrue(request.getValidator() instanceof InitialChoicesClientRequestValidator);
+    }
+
+    @Test
+    void testGetError(){
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player1,
+                Set.of(rightCard1, rightCard2),
+                twoResourcesChosen
+            ), gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player2,
+                Set.of(rightCard1, rightCard2),
+                fiveResourcesChosen
+            ), gameManager
+        ).isEmpty());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player2,
+                Set.of(rightCard1, rightCard2),
+                twoResourcesChosen
+            ), gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player1,
+                Set.of(rightCard1, rightCard2),
+                invalidStorageWithOneResourceChosen
+            ), gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player2,
+                Set.of(rightCard1),
+                fiveResourcesChosen
+            ), gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player2,
+                Set.of(differentCard, rightCard2),
+                fiveResourcesChosen
+            ), gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new InitialChoicesClientRequest(
+                player2,
+                Set.of(differentCard, rightCard1),
+                twoResourcesChosen
+            ), gameManager
+        ).isPresent());
+    }
 }
--- src/test/java/it/polimi/ingsw/network/clientrequest/validator/ProductionActionClientRequestValidatorTest.java
+++ src/test/java/it/polimi/ingsw/network/clientrequest/validator/ProductionActionClientRequestValidatorTest.java
@@ -1,89 +1,117 @@
 package it.polimi.ingsw.network.clientrequest.validator;
-//
 import static org.junit.jupiter.api.Assertions.*;
-//
-//import it.polimi.ingsw.network.clientrequest.ManageResourcesFromMarketClientRequest;
-//import it.polimi.ingsw.server.model.Player;
-//import it.polimi.ingsw.server.model.gamecontext.GameContext;
-//import it.polimi.ingsw.server.model.gamecontext.playercontext.PlayerContext;
-//import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
-//import it.polimi.ingsw.server.model.gameitems.Production;
-//import it.polimi.ingsw.server.model.gameitems.ResourceType;
-//import it.polimi.ingsw.server.model.gameitems.developmentcard.DevelopmentCard;
-//import it.polimi.ingsw.server.model.gamemanager.GameManager;
-//import it.polimi.ingsw.server.model.storage.ResourceStorage;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.Map;
-//import java.util.Set;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//
+import it.polimi.ingsw.network.clientrequest.ManageResourcesFromMarketClientRequest;
+import it.polimi.ingsw.network.clientrequest.ProductionActionClientRequest;
+import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.gamecontext.GameContext;
+import it.polimi.ingsw.server.model.gamecontext.playercontext.PlayerContext;
+import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
+import it.polimi.ingsw.server.model.gameitems.Production;
+import it.polimi.ingsw.server.model.gameitems.ResourceType;
+import it.polimi.ingsw.server.model.gameitems.developmentcard.DevelopmentCard;
+import it.polimi.ingsw.server.model.gamemanager.GameManager;
+import it.polimi.ingsw.server.model.storage.ResourceStorage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+
 class ProductionActionClientRequestValidatorTest {
-//
-//    @Mock
-//    GameManager gameManager;
-//
-//    @Mock
-//    GameContext gameContext;
-//
-//    @Mock
-//    PlayerContext playerContext;
-//
-//    @Mock
-//    GameItemsManager gameItemsManager;
-//
-//    @Mock
-//    ManageResourcesFromMarketClientRequestValidator validator;
-//
-//    @Mock
-//    Player player;
-//
-//    @Mock
-//    Production differentProduction;
-//
-//    Map<ResourceType, Integer> starResourceCost = Map.of(
-//        ResourceType.COINS, 1,
-//        ResourceType.SHIELDS, 2
-//    );
-//
-//    Map<ResourceType, Integer> starResourceReward = Map.of(
-//        ResourceType.STONES,3
-//    );
-//
-//    Set<Production> productionsThePlayerCanActivate;
-//    Iterator<Production> iter;
-//    Production rightProduction;
-//
-//    @BeforeEach
-//    void setUp() {
-//
-//        when(gameManager.getGameContext()).thenReturn(gameContext);
-//        when(gameContext.getPlayerContext(player)).thenReturn(playerContext);
-//        when(playerContext.getActiveProductions()).thenReturn(productionsThePlayerCanActivate);
-//        when(productionsThePlayerCanActivate.contains(differentProduction)).thenReturn(false);
-//    }
-//
-//    @Test
-//    void testGetError(){
-//        iter = productionsThePlayerCanActivate.iterator();
-//        rightProduction = iter.next();
-//
-//        ManageResourcesFromMarketClientRequest notActivateProductionRequest = new ManageResourcesFromMarketClientRequest(
-//
-//        )
-//
-//        assertTrue(validator.getErrorMessage(validRequest1, gameManager).isEmpty())
-//    }
+    @Mock
+    GameManager gameManager;
+
+    @Mock
+    GameContext gameContext;
+
+    @Mock
+    PlayerContext playerContext;
+
+    ProductionActionClientRequestValidator validator = new ProductionActionClientRequestValidator();
+
+    @Mock
+    Player player;
+
+    @Mock
+    Production differentProduction;
+
+    Map<ResourceType, Integer> starResourceCost = Map.of(
+        ResourceType.COINS, 1,
+        ResourceType.SHIELDS, 2
+    );
+
+    Map<ResourceType, Integer> starResourceReward = Map.of(
+        ResourceType.STONES,3
+    );
+
+    Set<Production> productionsThePlayerCanActivate = new HashSet<>();
+    Iterator<Production> iter;
+    Production rightProduction;
+
+    @BeforeEach
+    void setUp() {
+//lenient().when(gameManager.getGameItemsManager()).thenReturn(gameItemsManager);
+//        lenient().when(gameManager.getGameContext()).thenReturn(gameContext);
+//        lenient().when(gameContext.getPlayerContext(player)).thenReturn(playerContext);
+//        lenient().when(playerContext.getActiveLeaderCardsWhiteMarblesMarketSubstitutions()).thenReturn(playerMarbleSubstitution);
+//        lenient().when(storage1.canAddResources(any())).thenReturn(false);
+//        lenient().when(storage2.canAddResources(any())).thenReturn(true);
+//        lenient().when(playerContext.getTempStarResources()).thenReturn(2);
+//        lenient().when(playerContext.getTemporaryStorageResources()).thenReturn(Map.of(
+//            ResourceType.SHIELDS, 3,
+//            ResourceType.STONES,2
+//        ));
+        lenient().when(gameManager.getGameContext()).thenReturn(gameContext);
+        lenient().when(gameContext.getPlayerContext(player)).thenReturn(playerContext);
+        lenient().when(playerContext.getActiveProductions()).thenReturn(productionsThePlayerCanActivate);
+        lenient().when(productionsThePlayerCanActivate.contains(differentProduction)).thenReturn(false);
+    }
+
+    @Test
+    void getValidatorFromClientRequest() {
+        ProductionActionClientRequest request = new ProductionActionClientRequest(
+            player,
+            Set.of(rightProduction),
+            null,
+            null
+        );
+        assertTrue(request.getValidator() instanceof ProductionActionClientRequestValidator);
+    }
+
+    @Test
+    void testGetError(){
+        iter = productionsThePlayerCanActivate.iterator();
+        rightProduction = iter.next();
+
+//    public final Set<Production> productions;
+//    public final Map<ResourceType, Integer> starResourceCost;
+//    public final Map<ResourceType, Integer> starResourceReward;
+        assertTrue(validator.getErrorMessage(new ProductionActionClientRequest(
+            player,
+            Set.of(differentProduction),
+            null,
+            null
+        ),
+            gameManager
+        ).isPresent());
+
+        assertTrue(validator.getErrorMessage(new ProductionActionClientRequest(
+                player,
+                Set.of(differentProduction),
+                null,
+                null
+            ),
+            gameManager
+        ).isPresent());
+    }
 
 }
