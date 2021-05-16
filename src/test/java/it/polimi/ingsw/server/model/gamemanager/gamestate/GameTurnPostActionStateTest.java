663ec0d266ac63e9331798f07419d44bfd6209ae
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-05-16 16:47:14 +0200

    INGSW-17 fix details

--- src/test/java/it/polimi/ingsw/server/model/gamemanager/gamestate/GameTurnPostActionStateTest.java
+++ src/test/java/it/polimi/ingsw/server/model/gamemanager/gamestate/GameTurnPostActionStateTest.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.server.model.gamemanager.gamestate;
 
 import it.polimi.ingsw.network.clientrequest.EndTurnClientRequest;
 import it.polimi.ingsw.network.servermessage.EndTurnServerMessage;
+import it.polimi.ingsw.network.servermessage.GameUpdateServerMessage;
+import it.polimi.ingsw.network.servermessage.ServerMessage;
 import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.server.model.gamehistory.PostTurnFinalAction;
 import it.polimi.ingsw.server.model.gamehistory.SetupStartedAction;
@@ -29,14 +31,18 @@ class GameTurnPostActionStateTest extends GameStateTest{
     void testHandleRequestEndTurn(){
 
 //        assertFalse(state.isStateDone());
+//        Map<Player, GameUpdateServerMessage> initialMessagesFromServer = state.getInitialServerMessage();
+//        ServerMessage messageForPlayer = initialMessagesFromServer.get(player1);
+//        assertFalse(state.isStateDone());
+//
 //        EndTurnClientRequest request = new EndTurnClientRequest(player1);
-//        Map<Player, EndTurnServerMessage> handleRequestEndTurn = state.handleRequestEndTurn(request);
+//        Map<Player, ServerMessage> handleRequestEndTurn = state.handleRequestEndTurn(request);
 //
 //        assertTrue(state.isStateDone());
 //
 //        verifyGameHistoryActionAdded(PostTurnFinalAction.class);
 //
-//        verifyThatEveryPlayerGetsAllGameUpdates(player1, )
+//    verifyThatEveryPlayerGetsAllGameUpdates(player1, )
     }
 
 }
