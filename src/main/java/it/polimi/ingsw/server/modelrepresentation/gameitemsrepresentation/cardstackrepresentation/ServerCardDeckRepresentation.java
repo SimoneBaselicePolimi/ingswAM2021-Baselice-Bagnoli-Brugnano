e8dd3c35138b67ca4082df012c2352711ea01f40
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-05-17 22:11:13 +0200

    Fix constructors ServerCardDeckRepresentation and ServerPlayerOwnedDevelopmentCardDeckRepresentation.

--- src/main/java/it/polimi/ingsw/server/modelrepresentation/gameitemsrepresentation/cardstackrepresentation/ServerCardDeckRepresentation.java
+++ src/main/java/it/polimi/ingsw/server/modelrepresentation/gameitemsrepresentation/cardstackrepresentation/ServerCardDeckRepresentation.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.server.modelrepresentation.gameitemsrepresentation.cardstackrepresentation;
 
 import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
-import it.polimi.ingsw.server.model.gameitems.RegisteredIdentifiableItem;
 import it.polimi.ingsw.server.modelrepresentation.gameitemsrepresentation.ServerRegisteredIdentifiableItemRepresentation;
 
 import java.util.Stack;
@@ -13,14 +12,7 @@ public class ServerCardDeckRepresentation<C> extends ServerRegisteredIdentifiabl
      */
     public final Stack<C> cardDeck = new Stack<>();
 
-    /**
-     * CardDeckRepresentation constructor.
-     * @param itemID ID which identifies this specific Card Deck
-     * @param gameItemsManager a reference to gameItemsManager is needed to register the new CardDeck object
-     * (see {@link RegisteredIdentifiableItem})
-     */
-    protected ServerCardDeckRepresentation(String itemID, GameItemsManager gameItemsManager, Stack<C> cardDeck) {
+    public ServerCardDeckRepresentation(String itemID, GameItemsManager gameItemsManager) {
         super(itemID, gameItemsManager);
-        this.cardDeck = cardDeck;
     }
 }
--- src/main/java/it/polimi/ingsw/server/modelrepresentation/gameitemsrepresentation/cardstackrepresentation/ServerPlayerOwnedDevelopmentCardDeckRepresentation.java
+++ src/main/java/it/polimi/ingsw/server/modelrepresentation/gameitemsrepresentation/cardstackrepresentation/ServerPlayerOwnedDevelopmentCardDeckRepresentation.java
@@ -1,26 +1,11 @@
 package it.polimi.ingsw.server.modelrepresentation.gameitemsrepresentation.cardstackrepresentation;
 
 import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
-import it.polimi.ingsw.server.model.gameitems.RegisteredIdentifiableItem;
 import it.polimi.ingsw.server.modelrepresentation.gameitemsrepresentation.developmentcardrepresentation.ServerDevelopmentCardRepresentation;
-
-import java.util.Stack;
 
 public class ServerPlayerOwnedDevelopmentCardDeckRepresentation extends ServerCardDeckRepresentation<ServerDevelopmentCardRepresentation> {
 
-    /**
-     * CardDeckRepresentation constructor.
-     *
-     * @param itemID           ID which identifies this specific Card Deck
-     * @param gameItemsManager a reference to gameItemsManager is needed to register the new CardDeck object
-     *                         (see {@link RegisteredIdentifiableItem})
-     * @param cardDeck
-     */
-    protected ServerPlayerOwnedDevelopmentCardDeckRepresentation(
-        String itemID,
-        GameItemsManager gameItemsManager,
-        Stack<ServerDevelopmentCardRepresentation> cardDeck
-    ) {
-        super(itemID, gameItemsManager, cardDeck);
+    public ServerPlayerOwnedDevelopmentCardDeckRepresentation(String itemID, GameItemsManager gameItemsManager) {
+        super(itemID, gameItemsManager);
     }
 }
