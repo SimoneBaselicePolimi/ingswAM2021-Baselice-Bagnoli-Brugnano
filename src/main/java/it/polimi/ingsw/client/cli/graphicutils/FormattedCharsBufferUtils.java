4c20e43070c4ada54ba8059e30f48c1cab2f985f
Simone Baselice Mac <s.baselice@simonebaselice.it>
2021-05-31 17:35:45 +0200

    Implement FormattedCharsBufferUtils.createBufferForText()

--- src/main/java/it/polimi/ingsw/client/cli/graphicutils/FormattedCharsBufferUtils.java
+++ src/main/java/it/polimi/ingsw/client/cli/graphicutils/FormattedCharsBufferUtils.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.client.cli.graphicutils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FormattedCharsBufferUtils {
@@ -15,7 +16,64 @@ public class FormattedCharsBufferUtils {
         int bufferColSize,
         HorizontalAlignment horizontalAlignment
     ) {
-        return null;
+
+        List<Integer> newLineIndexes = new ArrayList<>();
+        for (int i = 0; i < text.size(); i++)
+            if(text.get(i).character == '\n')
+                newLineIndexes.add(i);
+
+        List<List<FormattedChar>> rows = new ArrayList<>();
+        int nextRowStartIndex = 0;
+        for (int nextRowEndIndex : newLineIndexes) {
+            rows.add(text.subList(nextRowStartIndex, nextRowEndIndex));
+            nextRowStartIndex = nextRowEndIndex + 1;
+        }
+        rows.add(text.subList(nextRowStartIndex, text.size()));
+
+        List<List<FormattedChar>> normalizedRows = new ArrayList<>();
+        for(List<FormattedChar> row : rows) {
+            List<FormattedChar> rowSectionToNormalize = row;
+            while (!rowSectionToNormalize.isEmpty()) {
+                if (rowSectionToNormalize.size() <= bufferColSize) {
+                    normalizedRows.add(rowSectionToNormalize);
+                    rowSectionToNormalize = List.of();
+                } else {
+                    int whitespaceCharIndex;
+                    for (
+                        whitespaceCharIndex = bufferColSize;
+                        whitespaceCharIndex > -1 &&
+                        rowSectionToNormalize.get(whitespaceCharIndex).character != ' ' &&
+                            rowSectionToNormalize.get(whitespaceCharIndex).character != '\t';
+                        whitespaceCharIndex--
+                    );
+                    if(whitespaceCharIndex != -1) {
+                        normalizedRows.add(rowSectionToNormalize.subList(0, whitespaceCharIndex));
+                        rowSectionToNormalize =
+                            rowSectionToNormalize.subList(whitespaceCharIndex+1, rowSectionToNormalize.size());
+                    } else {
+                        normalizedRows.add(rowSectionToNormalize.subList(0, bufferColSize));
+                        rowSectionToNormalize =
+                            rowSectionToNormalize.subList(bufferColSize, rowSectionToNormalize.size());
+                    }
+                }
+            }
+        }
+
+        FormattedCharsBuffer textBuffer = new FormattedCharsBuffer(normalizedRows.size(), bufferColSize);
+        for (int r = 0; r < normalizedRows.size(); r++) {
+            for (int i = 0; i < normalizedRows.get(r).size(); i++) {
+                int colIndex;
+                if(horizontalAlignment == HorizontalAlignment.LEFT)
+                    colIndex = i;
+                else if(horizontalAlignment == HorizontalAlignment.RIGHT)
+                    colIndex = bufferColSize - normalizedRows.get(r).size() + i;
+                else
+                    colIndex = (bufferColSize-normalizedRows.get(r).size())/2 + i;
+                textBuffer.setFormattedCharAtPosition(r, colIndex, normalizedRows.get(r).get(i));
+            }
+        }
+
+        return textBuffer;
     }
 
 }
