2425e6feec7f33a9494b21151f660cffe529b3d3
Simone Baselice Mac <s.baselice@simonebaselice.it>
2021-06-05 21:32:39 +0200

    Fix MapWithIdKeyDeserializer

--- src/main/java/it/polimi/ingsw/utils/serialization/deserializers/MapWithIdKeyDeserializer.java
+++ src/main/java/it/polimi/ingsw/utils/serialization/deserializers/MapWithIdKeyDeserializer.java
@@ -1,12 +1,11 @@
 package it.polimi.ingsw.utils.serialization.deserializers;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.BeanProperty;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import it.polimi.ingsw.server.model.gameitems.GameItemsManager;
 import it.polimi.ingsw.server.model.gameitems.IdentifiableItem;
 
@@ -19,11 +18,11 @@ public class MapWithIdKeyDeserializer<K extends IdentifiableItem, V>  extends Js
     implements ContextualDeserializer {
 
     private Class<K> typeOfKey;
-    private Class<V> typeOfValue;
+    private JavaType typeOfValue;
 
     public MapWithIdKeyDeserializer() {}
 
-    public MapWithIdKeyDeserializer(Class<K> typeOfKey, Class<V> typeOfValue) {
+    public MapWithIdKeyDeserializer(Class<K> typeOfKey, JavaType typeOfValue) {
         this.typeOfKey = typeOfKey;
         this.typeOfValue = typeOfValue;
     }
@@ -36,16 +35,21 @@ public class MapWithIdKeyDeserializer<K extends IdentifiableItem, V>  extends Js
     ) throws JsonMappingException {
         return new MapWithIdKeyDeserializer<>(
             (Class<K>) beanProperty.getType().containedType(0).getRawClass(),
-            (Class<V>) beanProperty.getType().containedType(1).getRawClass()
+            beanProperty.getType().containedType(1)
         );
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public Map<K, V> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
         throws IOException {
+        TypeFactory typeFactory = TypeFactory.defaultInstance();
+        JavaType typeOfMapWithIdKey = typeFactory.constructMapType(
+            HashMap.class,
+            typeFactory.constructFromCanonical(String.class.getName()),
+            typeOfValue
+        );
+        HashMap<String, V> mapWithIdKey = deserializationContext.readValue(jsonParser, typeOfMapWithIdKey);
         GameItemsManager manager = (GameItemsManager) deserializationContext.getAttribute("gameItemsManager");
-        Map<String, V> mapWithIdKey = jsonParser.readValueAs(new TypeReference<HashMap<String, V>>() {});
         return mapWithIdKey.entrySet().stream().collect(Collectors.toMap(
             (e) -> manager.getItem(typeOfKey, e.getKey()),
             Map.Entry::getValue
