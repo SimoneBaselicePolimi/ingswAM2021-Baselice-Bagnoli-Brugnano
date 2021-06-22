aa64ec36441d5c72e6d2b9eec732ddd0c0d2aec8
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2021-06-22 19:49:30 +0200

    fix details in FaithPath

--- src/main/java/it/polimi/ingsw/client/gui/fxcontrollers/FaithPath.java
+++ src/main/java/it/polimi/ingsw/client/gui/fxcontrollers/FaithPath.java
@@ -3,7 +3,11 @@ package it.polimi.ingsw.client.gui.fxcontrollers;
 import com.sun.prism.paint.Color;
 import it.polimi.ingsw.client.modelrepresentation.gamecontextrepresentation.faithrepresentation.ClientFaithPathRepresentation;
 import it.polimi.ingsw.client.modelrepresentation.gamecontextrepresentation.faithrepresentation.ClientVaticanReportSectionRepresentation;
+import it.polimi.ingsw.client.servermessage.PlayerNameAlreadyExistsServerMessage;
 import it.polimi.ingsw.configfile.FaithPathConfig;
+import it.polimi.ingsw.localization.Localization;
+import it.polimi.ingsw.server.model.Player;
+import it.polimi.ingsw.server.model.gamecontext.faith.PopeFavorCardState;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
 import javafx.fxml.FXML;
@@ -12,14 +16,20 @@ import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 import java.util.List;
+import java.util.Map;
 
 public class FaithPath extends GameScene {
 
     @FXML
-    GridPane faithPathContainer;
+    HBox faithPathContainer;
+
+    @FXML
+    VBox popeFavorCardsContainer;
 
     final ClientFaithPathRepresentation faithPath;
 
@@ -42,32 +52,63 @@ public class FaithPath extends GameScene {
     @FXML
     private void initialize() {
 
+        //faithPath section
         for (int c = 0; c < faithPathLength; c++) {
 
-            Pane cell = new Pane();
+            StackPane cell = new StackPane();
             cell.setStyle("-fx-border-color: black");
-            cell.setPrefSize(50, 100);
-            faithPathContainer.addColumn(c, cell);
+            cell.setPrefSize(200, 200);
+            faithPathContainer.getChildren().add(cell);
 
             Label numberPosition = new Label();
-            numberPosition.setFont(new Font(12));
+            numberPosition.setFont(new Font(14));
             numberPosition.setText(String.valueOf(c));
+            numberPosition.setAlignment(Pos.BOTTOM_RIGHT);
+            cell.getChildren().add(numberPosition);
+            StackPane.setAlignment(numberPosition, Pos.BOTTOM_CENTER);
 
-//            int victoryPoints = victoryPointsByPosition[c];
-//            Label victoryPointsByPosition = new Label();
-//            victoryPointsByPosition.setFont(new Font(10));
-//            victoryPointsByPosition.setText(String.valueOf(victoryPoints));
-//            victoryPointsByPosition.setAlignment(Pos.TOP_RIGHT);
-
-            cell.getChildren().addAll(numberPosition);
-
-            for(ClientVaticanReportSectionRepresentation vaticanReportSection : vaticanReportSections){
-                if (c >= vaticanReportSection.getSectionInitialPos() && c <= vaticanReportSection.getPopeSpacePos()){
-                    cell.setBackground(new Background(new BackgroundFill(Paint.valueOf("orange"), null, null)));
-                }
+            int victoryPoints = victoryPointsByPosition[c];
+            Label victoryPointsByPosition = new Label();
+            victoryPointsByPosition.setFont(new Font(12));
+            victoryPointsByPosition.setText(String.valueOf(victoryPoints));
+            cell.getChildren().add(victoryPointsByPosition);
+            StackPane.setAlignment(victoryPointsByPosition, Pos.TOP_RIGHT);
 
 
+            for (ClientVaticanReportSectionRepresentation vaticanReportSection : vaticanReportSections) {
+                if (c >= vaticanReportSection.getSectionInitialPos() && c <= vaticanReportSection.getPopeSpacePos())
+                    cell.setBackground(new Background(new BackgroundFill(Paint.valueOf("#7aa9ef"), null, null)));
             }
+        }
+
+        //popeCards section
+        for (int p = 0; p < clientManager.getGameContextRepresentation().getPlayersOrder().size(); p++){
+
+            HBox cardsBox = new HBox();
+
+            Label playerLabel = new Label();
+            playerLabel.setText(Localization.getLocalizationInstance().getString(
+                "client.gui.faithPath.playersLabel",
+                clientManager.getGameContextRepresentation().getPlayersOrder().get(p).getName()
+            ));
+            playerLabel.setFont(new Font(20));
+
+            for(int i = 0; i < faithPath.getPopeFavorCards().get(p).size(); i++){
+
+                StackPane card = new StackPane();
+                card.setStyle("-fx-border-color: black");
+                card.setPrefSize(100, 200);
+
+                Label cardStateLabel = new Label(faithPath.getPopeFavorCards().get(p).get(i).getLocalizedName());
+                cardStateLabel.setFont(new Font(12));
+                card.getChildren().add(cardStateLabel);
+                card.setAlignment(Pos.CENTER);
+
+                cardsBox.getChildren().add(i, card);
+            }
+
+            cardsBox.getChildren().add(0, playerLabel);
+            popeFavorCardsContainer.getChildren().add(p, cardsBox);
         }
 
     }
