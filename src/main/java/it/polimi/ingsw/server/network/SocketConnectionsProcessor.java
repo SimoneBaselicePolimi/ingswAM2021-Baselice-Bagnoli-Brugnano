416c0a9b41af137dae90ec2a57c62e00dc6c8f7d
Simone Baselice Mac <s.baselice@simonebaselice.it>
2021-06-30 02:02:28 +0200

    Add fallback map for key attachments in SocketConnectionsProcessor

--- src/main/java/it/polimi/ingsw/server/network/SocketConnectionsProcessor.java
+++ src/main/java/it/polimi/ingsw/server/network/SocketConnectionsProcessor.java
@@ -4,14 +4,8 @@ import it.polimi.ingsw.logger.ProjectLogger;
 import it.polimi.ingsw.server.controller.Client;
 
 import java.io.IOException;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.nio.channels.*;
+import java.util.*;
 
 public class SocketConnectionsProcessor implements Runnable {
 
@@ -21,6 +15,7 @@ public class SocketConnectionsProcessor implements Runnable {
 
     private Queue<SocketChannel> newSocketsQueue;
     final private Map<Client, SocketConnection> clientConnections;
+    final private Map<SelectableChannel, SocketConnection> channelToConnectionFallbackMap = new HashMap<>();
 
     private Selector readSelector;
     private Selector writeSelector;
@@ -73,14 +68,16 @@ public class SocketConnectionsProcessor implements Runnable {
 
             try {
                 newSocket.register(this.readSelector, SelectionKey.OP_READ, newConnection);
+                channelToConnectionFallbackMap.put(newSocket, newConnection);
+
+                inboundMessagesProcessor.processNewClientConnection(newConnection.getClient(), networkLayer);
+                newSocket = this.newSocketsQueue.poll();
+
             } catch (Exception c) {
                 ProjectLogger.getLogger().log(c);
                 throw c;
             }
 
-            inboundMessagesProcessor.processNewClientConnection(newConnection.getClient(), networkLayer);
-
-            newSocket = this.newSocketsQueue.poll();
         }
 
     }
@@ -97,7 +94,12 @@ public class SocketConnectionsProcessor implements Runnable {
             while(keyIterator.hasNext()) {
 
                 SelectionKey key = keyIterator.next();
-                SocketConnection connection = (SocketConnection) key.attachment();
+
+                SocketConnection connection;
+                if(key.attachment() != null)
+                    connection = (SocketConnection) key.attachment();
+                else
+                    connection = channelToConnectionFallbackMap.get(key.channel());
 
                 connection.readIncomingMessages().forEach(
                     m -> inboundMessagesProcessor.processNewMessage(m, networkLayer)
@@ -124,9 +126,13 @@ public class SocketConnectionsProcessor implements Runnable {
             while(keyIterator.hasNext()){
                 SelectionKey key = keyIterator.next();
 
-                Client client = (Client) key.attachment();
-
-                SocketConnection connection = clientConnections.get(client);
+                SocketConnection connection;
+                if(key.attachment() != null) {
+                    Client client = (Client) key.attachment();
+                    connection = clientConnections.get(client);
+                } else {
+                    connection = channelToConnectionFallbackMap.get(key.channel());
+                }
 
                 connection.flushOutboundMessages();
 
