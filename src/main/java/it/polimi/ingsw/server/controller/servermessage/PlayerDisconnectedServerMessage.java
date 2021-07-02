077ed384439fa9054beeeb5ee43cd84baef72647
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-07-02 20:35:45 +0200

    Add JsonProperty and JsonSubtypes to PlayerDisconnectedServerMessage

--- src/main/java/it/polimi/ingsw/server/controller/servermessage/PlayerDisconnectedServerMessage.java
+++ src/main/java/it/polimi/ingsw/server/controller/servermessage/PlayerDisconnectedServerMessage.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.server.controller.servermessage;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import it.polimi.ingsw.server.model.Player;
 import it.polimi.ingsw.utils.serialization.annotations.SerializeIdOnly;
 
@@ -8,7 +9,9 @@ public class PlayerDisconnectedServerMessage extends ServerMessage {
     @SerializeIdOnly
     public final Player player;
 
-    public PlayerDisconnectedServerMessage(Player player) {
+    public PlayerDisconnectedServerMessage(
+        @JsonProperty("player") Player player
+    ) {
         this.player = player;
     }
 
