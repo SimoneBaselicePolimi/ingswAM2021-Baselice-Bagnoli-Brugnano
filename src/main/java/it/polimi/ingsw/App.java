30212e0ec6105a97c9ef8e444263347311fced1c
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-07-02 23:03:34 +0200

    Add pom 2

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,34 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.client.network.ClientNotConnectedException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static it.polimi.ingsw.client.gui.GuiClient.startClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        try {
+            startClient();
+        } catch (IOException | ClientNotConnectedException e) {
+            e.printStackTrace();
+        }
+
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }
