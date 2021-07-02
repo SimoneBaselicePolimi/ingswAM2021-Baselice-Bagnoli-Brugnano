cb922cf94ef10f80283a348c2f705c6dc5900c0f
Matilde Brugnano <matilde.brugnano@mail.polimi.it>
2021-07-02 23:55:39 +0200

    final commit 3

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,34 +1,27 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.gui.GuiClient;
 import it.polimi.ingsw.client.network.ClientNotConnectedException;
-import javafx.application.Application;
-import javafx.stage.Stage;
+import it.polimi.ingsw.server.Server;
 
 import java.io.IOException;
-
-import static it.polimi.ingsw.client.gui.GuiClient.startClient;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException, ClientNotConnectedException, InterruptedException {
         System.out.println( "Hello World!" );
 
-        try {
-            startClient();
-        } catch (IOException | ClientNotConnectedException e) {
-            e.printStackTrace();
-        }
-
-        launch();
+        if(args[0].equals("server"))
+            Server.startServer();
+        else if(args[0].equals("cli"))
+            new Client().startClient();
+        else if(args[0].equals("gui"))
+            GuiClient.startClient();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-    }
 }
